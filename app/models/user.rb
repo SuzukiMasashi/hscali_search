73c3f763b1d51fc638f17f2f5c8aecd18e8583dd
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2015-02-26 02:37:29 +0900

    Fix parameter name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   class << self
     def find_or_create_from_auth_hash(auth_hash)
       provider  = auth_hash[:provider]
-      uid       = auth_hash[:id]
+      uid       = auth_hash[:uid]
       nickname  = auth_hash[:info][:nickname]
       image_url = auth_hash[:info][:image]
 

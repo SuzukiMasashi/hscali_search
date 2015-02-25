cc766c2d5de2f03e04b200db848bfa0d80c83228
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2015-02-25 17:05:25 +0900

    Add logout

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout',                  to: 'sessions#destroy', as: :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
